c939ce9884ad3611310494994e6a3149a2e81732
SawLwinOo <sawlwin@innovix-solutions.com>
2026-05-13 13:07:34 +0630

    FIX(sale_order_approval): scope all tests to custom addons

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -8,8 +8,25 @@ DB="${1:-odoo_test}"
 
 ODOO_CONF="${ODOO_CONF:-/etc/odoo/odoo.conf}"
 
+mapfile -t MODULES < <(
+  find addons -mindepth 2 -maxdepth 2 -name "__manifest__.py" \
+    -printf "%h\n" \
+    | sed "s#^addons/##" \
+    | sort
+)
+
+if [[ ${#MODULES[@]} -eq 0 ]]; then
+  echo "ERROR: no custom addons found under addons/"
+  exit 1
+fi
+
+MODULE_LIST="$(IFS=,; echo "${MODULES[*]}")"
+
 run_odoo_command \
   --config="$ODOO_CONF" \
   -d "$DB" \
+  -i "$MODULE_LIST" \
+  -u "$MODULE_LIST" \
+  --test-tags "$MODULE_LIST" \
   --test-enable \
   --stop-after-init
